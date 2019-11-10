c6cf93bbc0701db025b6c94811f0826b71f29618
adibfirman <dib.firman@gmail.com>
2019-11-10 12:26:42 +0700

    add: type declaration intersection observer

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState, Fragment, useEffect } from "react";
+
 import { makeID } from "../utils";
+import { IntersectionObserverEntry } from "./intersection-observer.d";
 
 export default function initMyFunc<T>(
   asyncComp: () => Promise<{ default: T }>
 ): Function {
   const getRandomString = makeID(5);
-  const className = `${getRandomString}_react-lazy-viewport_${getRandomString}`;
+  const className = `react-lazy-viewport-${getRandomString}`;
 
   function DefaultDOM(): JSX.Element {
     return <div className={className} style={{ visibility: "hidden" }} />;
@@ -13,20 +15,32 @@ export default function initMyFunc<T>(
 
   function ReactInitThis(): JSX.Element {
     const [theComponent, setComponent] = useState<T>();
+    const configs = { rootMargin: "0% 0% 10% 0%" };
+    const observer = new IntersectionObserver(callbackIO, configs);
+
+    function callbackIO(entries: IntersectionObserverEntry[]) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          loadComponent(() => {
+            observer.unobserve(entry.target);
+          });
+        }
+      });
+    }
+
+    function loadComponent(cb: Function) {
+      const init = asyncComp();
+      init.then(objData => {
+        cb();
+        setComponent(objData.default);
+      });
+    }
 
     useEffect(() => {
-      const configs = { rootMargin: "0% 0% 10% 0%" };
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            asyncComp().then(objData => setComponent(objData.default));
-          }
-        });
-      }, configs);
-
       const element = document.querySelector(`.${className}`);
+
       element && observer.observe(element);
-    }, []);
+    }, [observer]);
 
     return <Fragment>{theComponent || <DefaultDOM />}</Fragment>;
   }
---
+++ src/intersection-observer.d.ts
@@ -0,0 +1,16 @@
+export interface IntersectionObserverEntry {
+  boundingClientRect: ClientRect | DOMRect;
+  intersectionRatio: number;
+  intersectionRect: ClientRect | DOMRect;
+  isIntersecting: boolean;
+  rootBounds: ClientRect | DOMRect | null;
+  target: Element;
+  time: number;
+}
+
+interface IntersectionObserver {
+  disconnect(): void;
+  observe(target: Element): void;
+  takeRecords(): IntersectionObserverEntry[];
+  unobserve(target: Element): boid;
+}
